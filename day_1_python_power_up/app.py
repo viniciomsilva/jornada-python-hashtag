f0e79957fae36aa29eeb550af423050c6d77344f
Vinicio Silva <viniciomsilva@outlook.com.br>
2025-09-27 23:06:02 -0300

    refactor python power up app

---
+++ day_1_python_power_up/app.py
@@ -0,0 +1,25 @@
+from os import getcwd, path
+from time import sleep
+
+from automation import Automation
+
+
+if __name__ == "__main__":
+    auto = Automation(
+        db_filepath=path.join(getcwd(), "databases", "products.csv")
+    )
+
+    auto.open_system(
+        navigator="edge",
+        system_url="https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+    )
+    sleep(3)
+
+    auto.login(
+        email="my@mail.com",
+        password="qwe123@",
+        coord={"x": 795, "y": 380}
+    )
+    sleep(3)
+
+    auto.register(coord={"x": 725, "y": 250})
--- day_1_python_power_up/automation.py
+++ day_1_python_power_up/automation.py
@@ -1,72 +1,41 @@
 import pandas as pd
-import pyautogui as pa
+import pyautogui as pg
 
 
 class Automation:
 
-    def __init__(
-        self,
-        db_filepath: str,
-        system_url: str,
-        pause: float = 0.5,
-        login_screen: dict = {"x": 0, "y": 0},
-        registration_screen: dict = {"x": 0, "y": 0}
-    ):
-        pa.PAUSE = pause
-
-        self.system_url = system_url
+    def __init__(self, db_filepath: str, pause: float = 0.5):
+        pg.PAUSE = pause
         self.df = pd.read_csv(db_filepath)
-        self.mouse_pointer_positions: dict = {
-            "login_screen": login_screen,
-            "registration_screen": registration_screen
-        }
-
-        print(self.df.info())
 
     @staticmethod
     def view_mouse_pointer_position() -> None:
-        print(pa.position())
+        print(pg.position())
 
-    def open_system(self, navigate_name: str = "chrome") -> None:
-        pa.hotkey("win", "q")
-        pa.write(navigate_name)
-        pa.press("enter")
-        pa.write(self.system_url)
-        pa.press("enter")
+    def open_system(self, system_url: str, navigator: str = "chrome") -> None:
+        pg.hotkey("win", "q")
+        pg.typewrite(navigator)
+        pg.press("enter")
+        pg.hotkey("ctrl", "t")
+        pg.typewrite(system_url)
+        pg.press("enter")
 
-    def login(self, email: str, password: str) -> None:
-        pa.click(
-            x=self.mouse_pointer_positions["login_screen"]["x"],
-            y=self.mouse_pointer_positions["login_screen"]["y"]
-        )
-        pa.write(email)
-        pa.press("tab")
-        pa.write(password)
-        pa.press("tab")
-        pa.press("enter")
+    def login(self, email: str, password: str, coord: dict[str, int]) -> None:
+        pg.click(x=coord["x"], y=coord["y"])
+        pg.typewrite(email)
+        pg.press("tab")
+        pg.typewrite(password)
+        pg.press("tab")
+        pg.press("enter")
 
-    def register(self) -> None:
-        for row in self.df.index:
-            pa.click(
-                x=self.mouse_pointer_positions["registration_screen"]["x"],
-                y=self.mouse_pointer_positions["registration_screen"]["y"]
-            )
-            pa.write(self.df.loc[row, "codigo"])
-            pa.press("tab")
-            pa.write(self.df.loc[row, "marca"])
-            pa.press("tab")
-            pa.write(self.df.loc[row, "tipo"])
-            pa.press("tab")
-            pa.write(str(self.df.loc[row, "categoria"]))
-            pa.press("tab")
-            pa.write(str(self.df.loc[row, "preco_unitario"]))
-            pa.press("tab")
-            pa.write(str(self.df.loc[row, "custo"]))
-            pa.press("tab")
+    def register(self, coord: dict[str, int]) -> None:
+        for index in self.df.index:
+            pg.click(x=coord["x"], y=coord["y"])
 
-            if not pd.isna(self.df.loc[row, "obs"]):
-                pa.write(self.df.loc[row, "obs"])
+            for column in self.df.columns:
+                if not pd.isna(self.df.loc[index, column]):
+                    pg.typewrite(str(self.df.loc[index, column]))
+                pg.press("tab")
 
-            pa.press("tab")
-            pa.press("enter")
-            pa.hotkey("ctrl", "home")
+            pg.press("enter")
+            pg.hotkey("ctrl", "home")
